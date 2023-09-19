8759085d0b86755515df497b7d4d7cae0b5623f9
DFDRDODM95 <130643312+DFDRDODM95@users.noreply.github.com>
2023-09-19 19:40:07 +0900

    [Bronze V] Title: 아!, Time: 132 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/4999. 아！/아！.js
+++ 백준/Bronze/4999. 아！/아！.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const input = fs.readFileSync(0, 'utf8').toString().split('\n');
+const fs = require("fs");
+const input = fs.readFileSync(0, "utf8").toString().split('\n');
 
 const [ans, rq] = input;
 
