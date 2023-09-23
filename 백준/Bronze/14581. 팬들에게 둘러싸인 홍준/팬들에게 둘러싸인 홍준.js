1306f06d4ab9c232a050b2b51b90150a0c2219e3
DFDRDODM95 <130643312+DFDRDODM95@users.noreply.github.com>
2023-09-23 14:58:20 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 120 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').toString().trim();
+
+console.log(":fan::fan::fan:");
+console.log(":fan::" + input + "::fan:");
+console.log(":fan::fan::fan:");
