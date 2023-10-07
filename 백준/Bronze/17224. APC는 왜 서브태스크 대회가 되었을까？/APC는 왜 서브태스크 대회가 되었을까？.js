36eaee73e9fe509b21e547e3ec6d59d62293a9d5
DFDRDODM95 <130643312+DFDRDODM95@users.noreply.github.com>
2023-10-07 14:10:48 +0900

    [Bronze I] Title: APC는 왜 서브태스크 대회가 되었을까?, Time: 128 ms, Memory: 9636 KB, Score: 140 point -BaekjoonHub

--- 백준/Bronze/17224. APC는 왜 서브태스크 대회가 되었을까？/APC는 왜 서브태스크 대회가 되었을까？.js
+++ 백준/Bronze/17224. APC는 왜 서브태스크 대회가 되었을까？/APC는 왜 서브태스크 대회가 되었을까？.js
@@ -7,12 +7,12 @@ const quiz = inputData.slice(1, n + 1).map( str => str.split(' ').map( item => p
 let score = 0;
 let count = 0;
 for (let [key, value] of quiz.entries()) {
+    if ( count >= k ) break;
     if ( l >= value[0] ) { 
         count++;
         score += 100;
         if ( l >= value[1] ) score += 40;
     }
-    if ( count >= k ) break;
 }
 
 console.log(score);
