984c8baf527e5872b5e9a8e4e8286f3cf70e5c87
DFDRDODM95 <130643312+DFDRDODM95@users.noreply.github.com>
2023-11-01 23:16:14 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 448 ms, Memory: 55764 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').toString().trim();
+const inputData = input.split('\n');
+const amount = parseInt(inputData[0]);
+const tc = inputData.slice(1, amount + 1).map(str => str.split(' ').map(item => parseInt(item)) );
+
+function comp(l, r) {
+    if (l[1] < r[1]) {
+        return -1;
+    }
+    else if (l[1] > r[1]) {
+        return 1;
+    }
+    else {
+        if (l[0] < r[0]){
+            return -1;
+        }
+    }
+    return 1;
+}
+
+const answer = tc.sort( (a, b) => comp(a, b) ).map(v => v.join(' '));
+console.log(answer.join('\n'));
