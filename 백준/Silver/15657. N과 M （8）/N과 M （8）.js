983129321862084826c32488cfe3f02d74717a85
DFDRDODM95 <130643312+DFDRDODM95@users.noreply.github.com>
2023-11-16 19:22:27 +0900

    [Silver III] Title: N과 M (8), Time: 180 ms, Memory: 12644 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const input = fs.readFileSync(0, 'utf8').toString().trim();
+const inputData = input.split('\n').map(str => str.split(' ').map(item => parseInt(item)) );
+const [n, m] = inputData[0];
+const tc = inputData[1].sort( (a, b) => a - b );
+
+let arr = new Array(m).fill(0);
+let rst = '';
+function solution(k, a) {
+    if (k === m) {
+        rst += arr.join(' ') + '\n';
+        return;
+    }
+    for (let i = a; i <= n; i++) {
+        arr[k] = tc[i - 1];
+        solution(k + 1, i);
+    }
+}
+
+solution(0, 1);
+console.log(rst);
